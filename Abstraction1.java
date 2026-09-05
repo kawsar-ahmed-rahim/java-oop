7b0f60dc877e01235ecc2d2c732507f353fc1c67
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-05 13:29:43 +0600

    till now

---
+++ Abstraction1.java
@@ -0,0 +1,23 @@
+
+abstract class Animal {
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    public void walk() {
+        System.out.println("walks on 4 legs");
+    }
+}
+
+class Chicken extends Animal {
+    public void walk() {
+        System.out.println("walks on 2 legs");
+    }
+}
+
+public class Abstraction1 {
+    public static void main(String args[]) {
+        Horse horse = new Horse();
+        horse.walk();
+    }
+}
